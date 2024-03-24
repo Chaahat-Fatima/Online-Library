8052e2fa75bd11f167ae07f812111edc5ee5a9bd
Isbah <Isbah@DESKTOP-JFD7S2E>
2024-03-24 23:26:37 +0500

    Add project files.

---
+++ Models/MajuLibContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Online_Library.Models;
+
+public partial class MajuLibContext : DbContext
+{
+    public MajuLibContext()
+    {
+    }
+
+    public MajuLibContext(DbContextOptions<MajuLibContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Table> Tables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-JFD7S2E\\SQLEXPRESS;Database=MAJU_lib;Trusted_Connection=True;trustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Table>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__Table__490D1AE1D4A264EE");
+
+            entity.ToTable("Table");
+
+            entity.Property(e => e.BookId)
+                .ValueGeneratedNever()
+                .HasColumnName("book_id");
+            entity.Property(e => e.Author)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("author");
+            entity.Property(e => e.Genre)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("genre");
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("isbn");
+            entity.Property(e => e.PublicationYear).HasColumnName("publication_year");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("title");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Online_Library.Models;
+
+public partial class Table
+{
+    public int BookId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public string? Genre { get; set; }
+
+    public int? PublicationYear { get; set; }
+
+    public string? Isbn { get; set; }
+}
